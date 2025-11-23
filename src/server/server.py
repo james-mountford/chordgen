9cc9e4e47c5764ab95cbc0b63a4d7c1399c9d60f
james-mountford <jimmy_mountford@yahoo.com>
2025-11-23 02:34:08 -0600

    Added a compiled version of the mixer; added requirements.txt; made some small changes to the flask app and the mixer

--- src/server/server.py
+++ src/server/server.py
@@ -1,4 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
+import sys
+import uuid
+
+# add the root 'python' folder to sys.path
+sys.path.append(
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "..", "python")
+    )
+)
+from mixer import mixer as mixer
+print("mixer module:", mixer)
+print("mixer __file__:", getattr(mixer, "__file__", None))
+print("mixer dir:", dir(mixer))
 
 app = Flask(__name__)
 
@@ -6,6 +20,15 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, world!</p>"
 
-@app.route("/api/generate-progression-audiofile")
+@app.route("/")
+def index():
+    title = "Chordgen"
+    return render_template('index.html', title=title)
+
+@app.route("/api/generate-sample-progression")
 def mix_chords():
+    file_name = str(uuid.uuid4()) + '.wav'
+    sample_progression = [['C3', 'E3', 'G3', 'C4'], ['G3', 'B3', 'D4'], ['C3', 'E3', 'G3', 'C4']]
+    file_path = mixer.render_chord_progression(sample_progression, file_name)
+    print(file_path)
     return "<p>Placeholder for the actual stout stream...</p>"
